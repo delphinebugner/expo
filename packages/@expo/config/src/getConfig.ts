c84c8718fd3693c6be2e376d1d67cab2eeb18adb
Delphine Bugner <delphine@dejablue.energy>
2024-07-05 19:04:35 +0200

    [config] Fixed error in error in readConfigFile

--- packages/@expo/config/src/getConfig.ts
+++ packages/@expo/config/src/getConfig.ts
@@ -14,10 +14,10 @@ function readConfigFile(configFile: string, context: ConfigContext): null | Dyna
   }
   try {
     return evalConfig(configFile, context);
-  } catch (error: any) {
+  } catch (e: any) {
+    const error = new Error(`Error reading Expo config at ${configFile}:\n\n${e.message}`);
     // @ts-ignore
     error.isConfigError = true;
-    error.message = `Error reading Expo config at ${configFile}:\n\n${error.message}`;
     throw error;
   }
 }
